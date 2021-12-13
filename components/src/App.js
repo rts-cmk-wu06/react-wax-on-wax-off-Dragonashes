5a4802e6f612a6676b5a05913ef3cbab1d87209c
Dragonashes <dlsfs@tutanota.de>
2021-12-13 10:36:24 +0100

    initial images

--- components/src/App.js
+++ components/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import Rectangle_2 from './assets/Rectangle_2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} />
+
+      <img src={Rectangle_2} />
+
+      <img src={logo} />
+      <img src={logo} />
+
     </div>
   );
 }
--- setup.md
+++ setup.md
@@ -2,3 +2,6 @@ npx create-react-app components
 
 cd components
 npm start
+
+
+Web1280_1.svg er for stor til github, find svg elementerne
